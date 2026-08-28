d3c390b77c5c124c3220dd113beca2b26e993c4d
rkuskopf <rkuskopf@users.noreply.github.com>
2026-08-28 04:07:43 +0000

    Deploy preview for PR 87 🛫

--- pr-preview/pr-87/next-app/next.config.ts
+++ pr-preview/pr-87/next-app/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   agentRules: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors https://app.storyblok.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
